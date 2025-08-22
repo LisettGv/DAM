e8a92821ab2c976aa307a2acc89d6da4bdbf70d4
LisettGv <li.granadillo@duocuc.cl>
2025-08-22 15:42:42 -0400

    programacion centralizada

--- nullsafety.kt
+++ nullsafety.kt
@@ -1,3 +1,4 @@
+/*
 fun main(){
     println("inicio de programa")
 
@@ -12,10 +13,10 @@ fun main(){
     println ("mul: $mul")
     println ("div: $div")
     println ("mod: $mod")
-
+*/
 
     /*Creando Funciones*/
-
+/*
     fun suma(a:Int,b:Int):int{
         return a+b;
     }
@@ -24,7 +25,7 @@ fun main(){
         return a-b;
     }
 
-    fun nul(a:Int,b:Int):int{
+    fun mul(a:Int,b:Int):int{
         return a*b;
     }
 
@@ -36,4 +37,42 @@ fun main(){
         return a%b;
     }
 }
+*/
+
+/*Programación Centralizada*/
+
+fun main(){
+    println("Inicio de Programa")
+
+    val a = 5
+    val b = 3
+
+    sumar(a,b)
+    sumar(9,6)
+    sumar(4,7)
+    sumar(93,1)
+}
+
+fun sumar(a:Int,b:Int){
+    println("Programa de Sumas Extremas - Voy a sumar $a con $b=")
+    println(a+b)
+}
+
+fun res(a:Int,b:Int){
+    println("Programa de Sumas Extremas - Voy a restar $a con $b=")
+    println(a-b)
+}
+
+fun mul(a:Int,b:Int){
+    println("Programa de Sumas Extremas - Voy a restar $a con $b=")
+    println(a*b)
+}
+
+
+
+
+
+
+
+
 
