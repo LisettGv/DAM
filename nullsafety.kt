51d1b57de30f115201ecd0ee448d349a377fcbc7
LisettGv <li.granadillo@duocuc.cl>
2025-08-22 15:05:03 -0400

    nullsafety clase app moviles 22 ago 2025

---
+++ nullsafety.kt
@@ -0,0 +1,16 @@
+fun main(){
+    println("inicio de programa")
+
+    val sum = 5+3
+    val res = 5-3
+    val mul = 5*3
+    val div = 5/3
+    val mod = 5 % 3
+
+    println ("sum: $sum")
+    println ("res: $res")
+    println ("mul: $mul")
+    println ("div: $div")
+    println ("mod: $mod")
+
+}
