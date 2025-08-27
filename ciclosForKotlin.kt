2c431eb1e8635f8e43b064c295e253c63ac16c5f
lisettGv <li.granadillo@duocuc.cl>
2025-08-27 12:21:38 -0400

    Ejercicios Ciclo FOR Kotlin

--- ciclosForKotlin.kt
+++ ciclosForKotlin.kt
@@ -1,9 +1,34 @@
+
 fun main(){
 
     for(i in 1 .. 10){
         println("Vamos en el valor $i")
-
     }
-
-
 }
+
+
+
+/*Ejercicio 1*/
+
+fun main() {
+
+    for (i in 1..20) {
+        if (i==5|| i in 11..19){
+            continue
+        }
+        println(i)
+    }
+}
+
+
+/*Ejercicio 2*/
+
+fun main() {
+
+    for (i in 2..20) {
+        if(i % 2 ==0){
+            println(i)
+        }
+    }
+}
+
