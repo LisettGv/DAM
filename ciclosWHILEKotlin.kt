2e5167f056d4257ec52f856ca8073593e9ac899a
lisettGv <li.granadillo@duocuc.cl>
2025-08-27 12:45:19 -0400

    Ejercicios Ciclo WHILE Kotlin

--- ciclosWHILEKotlin.kt
+++ ciclosWHILEKotlin.kt
@@ -1,7 +1,21 @@
+/*
 fun main(){
     var i =1;
     do{
         println(i)
         i++
     }while(i <=10)
+}
+*/
+
+fun main(){
+    var i =1;
+    var ciclos = 1;
+    do{
+        ciclos++;
+        val randomNumber = (0..10).random()
+        i = i + randomNumber;
+        println("ciclo $ciclos : $i")
+
+    }while(i <=300)
 }
