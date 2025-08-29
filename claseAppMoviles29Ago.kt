cb758002c9533239e6fe1f1cacd943c4bcb96797
LisettGv <li.granadillo@duocuc.cl>
2025-08-29 15:19:02 -0400

    Operaciones en Colecciones

---
+++ claseAppMoviles29Ago.kt
@@ -0,0 +1,107 @@
+/*fun main(){
+    var lista=lostOf(1,2,5)
+    println("lista:$lista")
+
+    var conjunto=setOf(1,2,5)
+    println("conjunto= $conjunto")
+
+    var mapa=mapOf("nombre" to "juan", "edad" to 9, "dolares" to 1000)
+    println("mapa: $mapa")
+}
+*/
+
+
+/*fun main(){
+    var lista=mutableListOf(1,2,5,8,1'0,33,67,100)
+            lista.add(5)
+            lista.add(5)
+            lista.add(5)
+            println("lista: $lista")
+
+        var filtrado=lista.filter{ it % 2 ==0}
+    println("filtrado: $filtrado")
+
+    var doble=lista.map{ it +1}
+    println("doble: $doble")
+
+    var sumatoria=lista.sum()
+    println("sumatoria: $sumatoria")
+
+    var buscaElPar=lista.find{ it % 2! ==0}
+    println("buscaElPar: $buscaElPar")
+
+    var primerElemento=lista.first()
+    println("primerElemento: $primerElemento")
+
+    var ultimoElemento=lista.last()
+    println("ultimoElemento: $ultimoElemento")
+
+}
+*/
+
+
+/*fun main(){
+    var lista=mutableListOf(1,2,5,8,1'0,33,67,100)
+            println("lista: $lista")
+
+        var filtrado=lista.filter{it % 2 ==0}
+    println("filtrado: $filtrado")
+
+    var doble=lista.map{it +1}
+    println("doble: $doble")
+
+    var sumatoria=lista.sum()
+    println("sumatoria: $sumatoria")
+
+    var buscaElPar=lista.find{it % 2! ==0}
+    println("buscaElPar: $buscaElPar")
+
+    var primerElemento=lista.first()
+    println("primerElemento: $primerElemento")
+
+    var ultimoElemento=lista.last()
+    println("ultimoElemento: $ultimoElemento")
+
+}
+*/
+
+//FOR
+fun main(){
+    var lista=mutableListOf(1,2,5,)
+            println("lista: $lista")
+
+        for (i in lista){
+            println("El Elemento de la Lista es: $i")
+        }
+
+        var informacion = mapOf("Nombre" to "Fernanda","Edad" to 29)
+        informacion.forEach {
+            (llave,valor)-> println("La Llave es $llave y El Valor es $valor")
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
